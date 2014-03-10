2fac775f2f51f0514fd1a00e8975ed43194c95c9
Ekatya <evglushenok@mail.ru>
2014-03-10 12:09:37 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MegashopT::Application.config.secret_key_base = '3fac4ecf7409c49e31f6db549307d483f5311023845efb96f26fd7ea87e02613d881e7261088182048c89297a4821be935fcf24630c71a31ee1fd2eeeaa1e991'
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Product do
+	let(:product){@product=Product.create(name: 'Mirror',price: 20, description: )}
+
+
+
+
+
+
+
+  #pending "add some examples to (or delete) #{__FILE__}"
+end
